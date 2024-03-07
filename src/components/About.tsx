25e9ef06bd3bcca22ae809fe3c7c2f51d141a15d
dillonzellis <dillonzellis@gmail.com>
2024-03-07 11:01:02 -0500

    feat: add About and SectionHeading components

---
+++ src/components/About.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { SectionHeading } from "./SectionHeading";
+
+export const About = () => {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nunc sed augue lacus
+        viverra vitae congue eu consequat ac. Mauris vitae ultricies leo integer
+        malesuada. Viverra adipiscing at in tellus integer feugiat scelerisque.
+        Facilisis gravida neque convallis a cras semper auctor neque vitae. Mi
+        eget mauris pharetra et ultrices neque.
+      </p>
+    </motion.section>
+  );
+};
