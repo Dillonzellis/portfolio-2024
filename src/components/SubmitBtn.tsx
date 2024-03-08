9002bab5aad5b8117a00b4a0d34471f928ea332c
dillonzellis <dillonzellis@gmail.com>
2024-03-08 11:08:59 -0500

    feat: add contact form

---
+++ src/components/SubmitBtn.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaPaperPlane } from "react-icons/fa";
+
+export default function SubmitBtn() {
+  return (
+    <button
+      type="submit"
+      className="group flex h-[3rem] w-[8rem] items-center justify-center gap-2 rounded-full bg-gray-900 text-white outline-none transition-all hover:scale-110 hover:bg-gray-950 focus:scale-110 active:scale-105 disabled:scale-100 disabled:bg-opacity-65"
+    >
+      Submit{" "}
+      <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:-translate-y-1 group-hover:translate-x-1" />{" "}
+    </button>
+  );
+}
