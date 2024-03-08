ada5a357abddcaeb0a3dda62c817f7990dcc3aaa
dillonzellis <dillonzellis@gmail.com>
2024-03-08 11:53:36 -0500

    feat: add basic footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy;{new Date().getFullYear()} Dillonzellis. All rights reserved.
+      </small>
+      <p className="text-xs">this is a portfolio site</p>
+    </footer>
+  );
+};
