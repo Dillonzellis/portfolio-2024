f9d98923ea3c05bbf89d6fd248c6ef83d39c00fe
dillonzellis <dillonzellis@gmail.com>
2024-03-11 13:57:09 -0400

    style: add dark classes to experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,11 +9,13 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
+import { useTheme } from "@/context/theme-context";
 
 // TODO: Fix or replace this timeline component
 
 export const Experience = () => {
   const { ref } = useSectionInView("Experience");
+  const { theme } = useTheme();
 
   return (
     <section id="experience" ref={ref} className="mb-28 scroll-mt-28 sm:mb-40">
@@ -24,25 +26,30 @@ export const Experience = () => {
             <VerticalTimelineElement
               visible={true}
               contentStyle={{
-                background: "#f3f4f6",
+                background:
+                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
                 padding: "1.3rem 2rem",
               }}
               contentArrowStyle={{
-                borderRight: "0.4rem solid #9ca3af",
+                borderRight:
+                  theme === "light"
+                    ? "0.4rem solid #9ca3af"
+                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
               }}
               date={item.date}
               icon={item.icon}
               iconStyle={{
-                background: "white",
+                background:
+                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
                 fontSize: "1.5rem",
               }}
             >
               <h3 className="font-semibold capitalize">{item.title}</h3>
               <p className="!mt-0 font-normal">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700">
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                 {item.description}
               </p>
             </VerticalTimelineElement>
