be829cad6952634a58e4c6f2260b82e546f6edbb
dillonzellis <dillonzellis@gmail.com>
2024-03-11 13:48:08 -0400

    style: add dark classes section divider

--- src/components/SectionDivider.tsx
+++ src/components/SectionDivider.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 export const SectionDivider = () => {
   return (
     <motion.div
-      className="my-24 hidden h-16 w-1 rounded-full bg-gray-200 sm:block"
+      className="dark:bg-opacity-2 my-24 hidden h-16 w-1 rounded-full bg-gray-200 sm:block"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
