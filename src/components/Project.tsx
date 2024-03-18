f5d35729376381ab6278c4daf9ad64f88d3ed31b
dillonzellis <dillonzellis@gmail.com>
2024-03-18 13:32:51 -0400

    style: change project section tag

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -30,10 +30,10 @@ export const Project = ({
       }}
       className="group mb-3 last:mb-0 sm:mb-8"
     >
-      <section className="relative max-w-[42rem] overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition hover:bg-gray-200 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:h-[20rem] sm:pr-8 sm:group-even:pl-8">
+      <div className="relative max-w-[42rem] overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition hover:bg-gray-200 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:h-[20rem] sm:pr-8 sm:group-even:pl-8">
         <div className="flex h-full flex-col px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+          <p className="mt-2 text-sm leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
           <ul className="mt-4 flex flex-wrap gap-2 sm:mt-auto">
@@ -62,7 +62,7 @@ export const Project = ({
           group-even:group-hover:translate-y-3
           group-even:group-hover:rotate-2 sm:block"
         />
-      </section>
+      </div>
     </motion.div>
   );
 };
